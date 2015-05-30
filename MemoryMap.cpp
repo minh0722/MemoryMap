b24ad77e54791bd9595fec1c0c23688b3476b1f4
Minh Nguyen <minh0722@yahoo.com>
2015-05-30 23:08:06 +0300

    implemented sort by usage

--- MemoryMap.cpp
+++ MemoryMap.cpp
@@ -1,16 +1,21 @@
 #include "MemoryMap.h"
 #include <stdexcept>
+#include <algorithm>
 
 
 MemoryMap::MemoryMap(const char* file_path) {
 
 	m_file = fopen(file_path, "rb+");
 
-	if(!m_file)	{
+	if (!m_file)	{
 		throw std::exception("Can't open file");
 	}
 
 	memset(m_map, 0, PAGE_COUNT * sizeof(Page));
+
+	for (size_t i = 0; i < PAGE_COUNT; ++i) {
+		usage[i] = i;
+	}
 
 }
 
@@ -24,16 +29,17 @@ MemoryMap::~MemoryMap() {
 	fclose(m_file);
 }
 
-char& MemoryMap::operator[](const size_t index) {
-	if (index > fileSize()) {
+char& MemoryMap::operator[](const size_t byte_index) {
+	if (byte_index > fileSize()) {
 		throw std::out_of_range("Index out of range");
 	}
+	
+	int page_index = getPageOnIndex(byte_index);
+	++m_map[page_index].usage;
 
-	int page_index = getPageOnIndex(index);
+	sortPagesByUsage();
 
-
-
-	return m_map[page_index].buffer[index - m_map[page_index].from];
+	return m_map[page_index].buffer[byte_index - m_map[page_index].from];
 }
 
 
@@ -56,11 +62,9 @@ int MemoryMap::getPageOnIndex(size_t byteIndex) {
 		}
 	}
 
-	//
 	writePageToDisk(0);
 	deallocatePage(0);
 	allocatePage(0, getPageRange(byteIndex));
-
 	return 0;
 }
 
@@ -68,7 +72,7 @@ std::pair<size_t, size_t> MemoryMap::getPageRange(size_t byteIndex) {
 	size_t file_size = fileSize();
 
 	size_t from = (byteIndex / PAGE_SIZE) * PAGE_SIZE;
-	size_t to = file_size > from + PAGE_SIZE - 1 ? from + PAGE_SIZE - 1 : from + file_size - from - 1;
+	size_t to = file_size > from + PAGE_SIZE - 1 ? from + PAGE_SIZE - 1 : file_size  - 1;
 
 	return std::make_pair(from, to);
 }
@@ -81,14 +85,6 @@ void MemoryMap::writePageToDisk(size_t pageIndex) {
 }
 
 
-size_t MemoryMap::fileSize() {
-	size_t currPosition = ftell(m_file);
-	fseek(m_file, 0, SEEK_END);
-	size_t size = ftell(m_file);
-	fseek(m_file, currPosition, SEEK_SET);
-	return size;
-}
-
 void MemoryMap::allocatePage(size_t page_index, std::pair<size_t, size_t>& pageRange) {
 	m_map[page_index].from = pageRange.first;
 	m_map[page_index].to = pageRange.second;
@@ -96,6 +92,24 @@ void MemoryMap::allocatePage(size_t page_index, std::pair<size_t, size_t>& pageR
 	m_map[page_index].buffer = new char[m_map[page_index].getBufferSize()];
 	fseek(m_file, pageRange.first, SEEK_SET);
 	fread(m_map[page_index].buffer, sizeof(char), m_map[page_index].getBufferSize(), m_file);
+}
+
+void MemoryMap::sortPagesByUsage() {
+	std::sort(
+		usage, 
+		usage + PAGE_COUNT, 
+		[this](const int& l, const int& r){
+			return this->m_map[l].usage < this->m_map[r].usage;
+		}
+	);
+}
+
+size_t MemoryMap::fileSize() {
+	size_t currPosition = ftell(m_file);
+	fseek(m_file, 0, SEEK_END);
+	size_t size = ftell(m_file);
+	fseek(m_file, currPosition, SEEK_SET);
+	return size;
 }
 
 void MemoryMap::deallocateAllPages() {
